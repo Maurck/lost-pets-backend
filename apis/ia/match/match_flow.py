00ee844010d93b83fbe5628dc6e30136b45bbec1
Maurk <mauricio.lp@astrumla.com>
2022-07-24 21:10:45 -0500

    code: match class added

--- apis/ia/match/match_flow.py
+++ apis/ia/match/match_flow.py
@@ -1,26 +1,14 @@
-from models.user import User
-from models.pet import Pet
-from utils.utils import json_message, json_message_error_code 
+from utils.utils import jsonify
 
 
 class MatchFlow:
 
     def __call__(self, request):
-        user_id = request.args.get('id')    
-        user = User.objects(
-            id=user_id
-        ).first()
+        # Flujo de IA
+        reference = request.form["reference"]
+        img_from_user = request.form["img_from_user"]
 
-        if user is None:
-            return json_message_error_code("Usuario no encontrado")
 
-        user_pets = Pet.objects(
-            owner_id=user_id
-        )
-
-        if user_pets is not None:
-            user_pets.delete()
-        
-        user.delete()
-
-        return json_message("Usuario borrado")
+        return jsonify({
+            "percentage": "80%"
+        })
